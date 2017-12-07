57fa897a680c161ac5993b34060e306ba2c1acf9
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-12-08 05:51:00 +0800

    Update frame.m

--- frame.m
+++ frame.m
@@ -5,9 +5,9 @@ video=VideoReader(video_file);
 frame_number=floor(video.Duration * video.FrameRate);
 %% ????
 for i=1:frame_number
-    image_name=strcat('ccne1\',num2str(i));
+    image_name=strcat('ccne1\',num2str(i)); %create filename according to their sources
     image_name=strcat(image_name,'.jpg');
-    I=read(video,i);                               %????
-    imwrite(I,image_name,'jpg');                   %???
+    I=read(video,i);                               %generate images for each frame 
+    imwrite(I,image_name,'jpg');                   %save the images
     I=[];
 end
